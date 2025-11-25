40324d087728bc04fe7c838e042a5222f5ed3ab8
PradermN <pradermnafack@gmail.com>
2025-11-25 15:28:29 +0100

    Refactor code with functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +22,15 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -29,10 +39,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 5;
-                });
-                print('leftDiceNumber: $leftDiceNumber');
+                changeDiceFace();
               },
               child: Image(image: AssetImage('images/dice$leftDiceNumber.png')),
             ),
@@ -40,9 +47,9 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Dice button pressed');
+                changeDiceFace();
               },
-              child: Image(image: AssetImage('images/dice1.png')),
+              child: Image(image: AssetImage('images/dice$rightDiceNumber.png')),
             ),
           ),
         ],
